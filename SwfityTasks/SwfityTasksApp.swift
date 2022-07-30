ff353e29b90775d6d86e3a3402b836b8bd74a0da
Mico Infante <kinfante@isbx.com>
2022-07-30 15:43:09 +0800

    Initial Commit

---
+++ SwfityTasks/SwfityTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwfityTasksApp.swift
+//  SwfityTasks
+//
+//  Created by Mico Infante on 7/30/22.
+//
+
+import SwiftUI
+
+@main
+struct SwfityTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
